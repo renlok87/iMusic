d119b0233fe79fb990fef2a260d2877fc90c3470
renlok87 <renlok87@gmail.com>
2020-01-02 12:19:23 +0300

    add NetwirkService

---
+++ IMusic/NetworkService.swift
@@ -0,0 +1,38 @@
+import UIKit
+import Alamofire
+
+
+class NetworkService {
+    
+    func fetchTracks(searchText: String, competion: @escaping (SearchResponse?) -> Void)  {
+        let url = "https://itunes.apple.com/search"
+        let parameters = ["term":"\(searchText)",
+            "limit":"10",
+            "media":"music"]
+        
+        Alamofire.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
+            if let error = dataResponse.error {
+                print("Error received requestiong data: \(error.localizedDescription)")
+                competion(nil)
+                return
+            }
+            
+            
+            guard let data = dataResponse.data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let objects = try decoder.decode(SearchResponse.self, from: data)
+                print("objects: ", objects)
+                competion(objects)
+                
+            } catch let jsonError {
+                print("Failed to decode JSON", jsonError)
+                competion(nil)
+            }
+//            let someString = String(data: data, encoding: .utf8)
+//            print(someString ?? "")
+        }
+    }
+    
+}
--- IMusic/SearchViewController.swift
+++ IMusic/SearchViewController.swift
@@ -1,4 +1,3 @@
-
 import UIKit
 import Alamofire
 
@@ -7,9 +6,11 @@ struct TrackModel {
     var artistName: String
 }
 
-
 class SearchViewController: UITableViewController {
+    
+    var networkService = NetworkService()
     private var timer: Timer?
+    
     let searchController = UISearchController(searchResultsController: nil)
     
     var tracks = [Track]()
@@ -52,36 +53,10 @@ extension SearchViewController: UISearchBarDelegate {
         
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
-            let url = "https://itunes.apple.com/search?term=\(searchText)"
-            let parametrs = ["terms":"\(searchText)",
-                "limit": "10"]
-            Alamofire.request(url, method: .get, parameters: parametrs, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
-                if let error = dataResponse.error {
-                    print("Error received requestiong data: \(error.localizedDescription)")
-                    return
-                }
-                
-                
-                guard let data = dataResponse.data else { return }
-                
-                let decoder = JSONDecoder()
-                do{
-                    let objects = try decoder.decode(SearchResponse.self, from: data)
-                    print("objects", objects)
-                    self.tracks = objects.results
-                    self.tableView.reloadData()
-                }
-                    
-                catch let jsonError {
-                    print("Failed to decode JSON", jsonError)
-                }
-                let someString = String(data: data, encoding: .utf8)
-                print(someString ?? "")
+            self.networkService.fetchTracks(searchText: searchText) { [weak self] (searchResults) in
+                self?.tracks = searchResults?.results ?? []
+                self?.tableView.reloadData()
             }
-
         })
-        
-        
     }
-    
 }
